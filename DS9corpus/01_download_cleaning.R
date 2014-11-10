10c9d1e26cc1312365704400c9a8eaaa6f8e35bc
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2014-11-10 11:42:46 -0800

    download and save all of DS9

--- DS9corpus/01_download_cleaning.R
+++ DS9corpus/01_download_cleaning.R
@@ -2,6 +2,9 @@
 
 library(rvest)
 library(dplyr)
+library(staRtrek)
+library(tidyr)
+library(magrittr)
 
 # takes a show name, returns a function of episode number
 # StarTrek = TOS
@@ -15,6 +18,16 @@ get_script_url <- function(show){
   }
 }
 
-site <- get_script_url("NextGen")(252) %>%
-  html
+ds9_list <- episodes("DS9")
 
+DS9_episodes <- do.call(rbind, ds9_list) %>%
+  separate(Prodno., c("season", "episode"), remove = FALSE) %>%
+  mutate(episode = ifelse(episode == 721, 401, episode)) # for some reason, "Emissary" has a different number!
+
+## get the websites
+sites <- DS9_episodes %>% 
+  extract2("episode") %>%
+  lapply(get_script_url("DS9")) %>%
+  lapply(html)
+
+save(sites, file = "./DS9corpus/01_DS9_websites.Rdata")
